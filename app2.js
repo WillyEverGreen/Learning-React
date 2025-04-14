12fda95a99b851d5a036662c8a33c273385319a6
Sai Balkawade <saibalkawade10@gmail.com>
2025-04-14 21:41:47 +0530

    #1

---
+++ app2.js
@@ -0,0 +1,14 @@
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "This is the h1"),
+    React.createElement("h2", {}, "This is the h2"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "This is the h1"),
+    React.createElement("h2", {}, "This is the h2"),
+  ]),
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
